f05fea6cba69194de2db9013d502731693b771b4
SHAKEEL AHMED <shakeelahmadprogrammer@gmail.com>
2024-09-08 02:19:13 -0700

    change local host

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Travel-Agency-Website/",
+  // base: "/Travel-Agency-Website/",
   plugins: [react()],
 })
